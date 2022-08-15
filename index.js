2f6e2939b427bf977f850a2d20eb9d19999c08ea
Aynur Sönmez <80594556+aynur-123@users.noreply.github.com>
2022-08-15 14:20:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function pink(){
+    document.getElementById('box').style.backgroundColor="pink"
+}
+function yellow(){
+    document.getElementById('box').style.backgroundColor="yellow"
+}
+function blue(){
+    document.getElementById('box').style.backgroundColor="blue"
+}
